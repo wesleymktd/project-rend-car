11a36efdcd1d9cf07891863f8a9faffa0c4d0ab3
wesleymktd <wesleymktd@gmail.com>
2023-07-10 13:59:56 -0300

    classe Vehicle criado test1 req3

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -4,4 +4,13 @@ namespace RentCars.Models;
 //3 - Crie a classe `Vehicle`
 public class Vehicle
 {
+    string Brand {get; set;} = "";
+    string Model {get; set;} = "";
+    decimal Price {get; set;}
+    FuelType fuel {get; set;}
+    int EngineCapacity {get; set;}
+    Color MainColor {get; set;}
+    int Year {get; set;}
+    double PricePerDay {get; set;}
+    bool IsRented {get; set;} = false;
 }
